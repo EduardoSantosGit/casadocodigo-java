bb021475f9d315801e4086f1ee7dc2be7204bc27
eduardo <eduardosantos058@gmail.com>
2017-03-26 10:17:02 -0300

    finalizacao modulo 1

--- src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.format.datetime.DateFormatter;
 import org.springframework.format.datetime.DateFormatterRegistrar;
 import org.springframework.format.support.DefaultFormattingConversionService;
 import org.springframework.format.support.FormattingConversionService;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -16,10 +17,11 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import br.com.casadocodigo.loja.controllers.HomeController;
 import br.com.casadocodigo.loja.daos.ProdutoDAO;
 import br.com.casadocodigo.loja.infra.FileSaver;
+import br.com.casadocodigo.loja.model.CarrinhoCompras;
 
 //habilita o uso do spring fala a classe inicial
 @EnableWebMvc
-@ComponentScan(basePackageClasses={HomeController.class,ProdutoDAO.class,FileSaver.class})
+@ComponentScan(basePackageClasses={HomeController.class,ProdutoDAO.class,FileSaver.class,CarrinhoCompras.class})
 public class AppWebConfiguration {
 	
 	//configura onde estao os arquivos 
@@ -29,6 +31,8 @@ public class AppWebConfiguration {
 		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
 		resolver.setPrefix("/WEB-INF/views/");
 		resolver.setSuffix(".jsp");
+		
+		resolver.setExposedContextBeanNames("carrinhoCompras");
 		
 		return resolver;
 	}
@@ -57,6 +61,11 @@ public class AppWebConfiguration {
 	@Bean
 	public MultipartResolver multipartResolver(){
 		return new StandardServletMultipartResolver();
+	}
+	
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
 	}
 	
 
--- src/main/java/br/com/casadocodigo/loja/model/Produto.java
+++ src/main/java/br/com/casadocodigo/loja/model/Produto.java
@@ -1,6 +1,7 @@
 package br.com.casadocodigo.loja.model;
 
 
+import java.math.BigDecimal;
 import java.util.Calendar;
 import java.util.List;
 
@@ -82,6 +83,33 @@ public class Produto {
 	public String toString() {
 		return "Produto [titulo=" + titulo + ", descricao=" + descricao + ", paginas=" + paginas + "]";
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
+	public BigDecimal precoPara(TipoPreco tipoPreco) {
+		return precos.stream().filter(preco -> preco.getTipo().equals(tipoPreco))
+				.findFirst().get().getValor();
+	}
+	
+	
 
 
 }
